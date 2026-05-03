ffdd39554c1bf14989fe76efe5d8c355ccc8aa90
sarvesh80028 <sarveshsingh80028@gmail.com>
2026-05-03 09:39:53 +0530

    Time: 0 ms (100%), Space: 42.7 MB (58.69%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        if(s.length()!=goal.length()) return false;
+         if(s.length()!=goal.length()) return false;
         String combined = s+s;
         return combined.contains(goal);
     }
