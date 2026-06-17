aaa3828c1e9ab980a2c94485a67377c3e3971b82
sarvesh80028 <sarveshsingh80028@gmail.com>
2026-06-17 17:56:38 +0530

    Time: 0 ms (100%), Space: 43 MB (73.7%) - LeetHub

--- 1748-sum-of-unique-elements/1748-sum-of-unique-elements.java
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.java
@@ -1,14 +1,13 @@
 class Solution {
     public int sumOfUnique(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length,sum = 0;
-        if(n==1) return nums[0];
-        int i = 0;
-        for(i=1;i<n-1;i++){
-            if(nums[i]!=nums[i+1] && nums[i]!=nums[i-1]) sum+=nums[i];
-        }
-        if(nums[0]!=nums[1]) sum+=nums[0];
-        if(i<n && nums[n-2]!=nums[n-1]) sum+=nums[n-1];
-        return sum;
+       int[]freq = new int[101];
+       int sum = 0;
+       for(int ele:nums){
+        freq[ele]++;
+       }
+       for(int ele:nums){
+        if(freq[ele]==1) sum+=ele;
+       }
+       return sum;
     }
 }
